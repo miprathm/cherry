e35a2650c67eb345362135beece3b540e2b20a8b
miprathm <1prathm@gmail.com>
2020-07-23 19:34:40 +0530

    CR:Prathm: Add web selenium dependencies

---
+++ src/main/java/org/test/cherry/HomePage.java
@@ -0,0 +1,13 @@
+package org.test.cherry;
+
+import org.openqa.selenium.WebDriver;
+
+public class HomePage {
+	
+	WebDriver webDriver;
+	
+	public HomePage(WebDriver driver) {
+		this.webDriver = driver;
+	}
+	
+}
