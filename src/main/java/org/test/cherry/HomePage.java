740c3fcf8ebfe2bc82f0613666e2d4818acbe6b6
miprathm <1prathm@gmail.com>
2020-07-23 19:37:49 +0530

    CR:Prathm: Add Deposite Element

--- src/main/java/org/test/cherry/HomePage.java
+++ src/main/java/org/test/cherry/HomePage.java
@@ -1,6 +1,9 @@
 package org.test.cherry;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage {
 	
@@ -8,6 +11,9 @@ public class HomePage {
 	
 	public HomePage(WebDriver driver) {
 		this.webDriver = driver;
+		PageFactory.initElements(driver, this);
 	}
 	
+	@FindBy(xpath="//div[contains(text(),'Deposits')]")
+	WebElement deposit;
 }
